443b8d4ba5a2715609425e0a0f6da10137a4c354
LoMbiChh <lombardi.matteo484@gmail.com>
2023-01-16 18:13:00 +0100

    add horizontal and vertical passage methods and create methods to add blocks in a direction

--- src/map.cpp
+++ src/map.cpp
@@ -4,7 +4,7 @@
 
 /* Methods */
 
-void add_block(map *map, int y, int x) {
+void add_block(map *map, int y, int x, int type, char symbol) {
     // check if block already exists in map
     int i = 0;
     bool found = false;
@@ -17,19 +17,47 @@ void add_block(map *map, int y, int x) {
 
     // add block if not already in map
     if (!found) {
-        block new_block = {y, x};
+        block new_block = {y, x, type, symbol};
         map -> blocks[map -> num_blocks] = new_block;
         map -> num_blocks++;
     }
 }
 
+void add_blocks_vertically(map *map, int y1, int y2, int x, int type, char symbol) {
+	// Precondition: y1 and y2 should not be out of bounds and y1<y2
+	// 0 < y1&&y2 < 20 && y1<y2
+	for (int i = y1; i < y2; i++) {
+		add_block(map, i, x, type, symbol);
+	}
+}
+
+void add_blocks_horizontally(map *map, int y, int x1, int x2, int type, int symbol) {
+	// Precondition: x1 and x2 should not be out of bounds and x1<x2
+	// 0 < x1&&x2 < 60 && x1<x2
+	for (int i = x1; i < x2; i++) {
+		add_block(map, y, i, type, symbol);
+	}
+}
+
+void add_hor_passage(map *map, int y, int x1, int x2) {
+	// Precondition: same as add_blocks_horizontally
+	add_block(map, y, x1, TYPE_WALL, '|');
+	add_block(map, y, x2, TYPE_WALL, '|');
+	add_blocks_horizontally(map, y, x1+1, x2, TYPE_BLANK, ' ');
+}
+
+void add_ver_passage(map *map, int y1, int y2, int x) {
+	// Precondition: same as add_blocks_vertically
+	add_block(map, y1, x, TYPE_WALL, '-');
+	add_block(map, y2, x, TYPE_WALL, '-');
+	add_blocks_vertically(map, y1+1, y2, x, TYPE_BLANK, ' ');
+}
+
 map *create_map() {
     map *new_map = new map;
 
-    add_block(new_map, 4, 5);
-    add_block(new_map, 4, 6);
-    add_block(new_map, 4, 7);
-    add_block(new_map, 4, 8);
-    
+    add_hor_passage(new_map, 19, 3, 9);
+    add_ver_passage(new_map, 3, 7, 59);
+
     return new_map;
 }
