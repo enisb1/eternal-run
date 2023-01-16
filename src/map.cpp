bc3b416ee67c6759eb24405d32931d06d7c0757b
Enis <b.encio.003@gmail.com>
2023-01-16 15:39:24 +0000

    create methods to add blocks in a direction

--- src/map.cpp
+++ src/map.cpp
@@ -23,26 +23,34 @@ void add_block(map *map, int y, int x, int type, char symbol) {
     }
 }
 
-void add_hor_passage(map *map, int y, int x1, int x2) {
-	// Precondition: x1 and x2 should not be out of bounds and x1<x2
-	// 0 < x1&&x2 < 60 && x1<x2
-	add_block(map, y, x1, TYPE_WALL, '|');
-	add_block(map, y, x2, TYPE_WALL, '|');
-
-	for (int i = x1+1; i < x2; i++) {
-		add_block(map, y, i, TYPE_BLANK, ' ');
+void add_blocks_vertically(map *map, int y1, int y2, int x, int type, char symbol) {
+	// Precondition: y1 and y2 should not be out of bounds and y1<y2
+	// 0 < y1&&y2 < 20 && y1<y2
+	for (int i = y1; i < y2; i++) {
+		add_block(map, i, x, type, symbol);
 	}
 }
 
+void add_blocks_horizontally(map *map, int y, int x1, int x2, int type, int symbol) {
+	// Precondition: x1 and x2 should not be out of bounds and x1<x2
+	// 0 < x1&&x2 < 60 && x1<x2
+	for (int i = x1; i < x2; i++) {
+		add_block(map, y, i, type, symbol);
+	}
+}
+
+void add_hor_passage(map *map, int y, int x1, int x2) {
+	// Precondition: same as add_blocks_horizontally
+	add_block(map, y, x1, TYPE_WALL, '|');
+	add_block(map, y, x2, TYPE_WALL, '|');
+	add_blocks_horizontally(map, y, x1+1, x2, TYPE_BLANK, ' ');
+}
+
 void add_ver_passage(map *map, int y1, int y2, int x) {
-	// Precondition: y1 and y2 should not be out of bounds and y1<y2
-	// 0 < y1&&y2 < 20 && y1<y2
+	// Precondition: same as add_blocks_vertically
 	add_block(map, y1, x, TYPE_WALL, '-');
 	add_block(map, y2, x, TYPE_WALL, '-');
-
-	for (int i = y1+1; i < y2; i++) {
-		add_block(map, i, x, TYPE_BLANK, ' ');
-	}
+	add_blocks_vertically(map, y1+1, y2, x, TYPE_BLANK, ' ');
 }
 
 map *create_map() {
