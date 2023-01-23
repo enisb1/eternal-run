fbd9cb5b5ffb0ec7b609809baa54513e6fe9b450
LoMbiChh <lombardi.matteo484@gmail.com>
2023-01-23 13:06:02 +0100

    reorganize main

--- src/main.cpp
+++ src/main.cpp
@@ -12,27 +12,27 @@ const int game_win_height = 20;
 const int game_win_width = 60;
 const int info_win_width = 18;
 
+map *maps[6];
 char title[25];
 int life = 3;
 int money = 0;
 
 /* Main method */
 
-int main() {
-    // start ncurses
-    initscr();
-    noecho();
-    cbreak();
-    
-    // start colors
+void create_colors() {
     if (has_colors()) start_color();
 
     init_pair(WALL_PAIR, COLOR_WHITE, COLOR_BLUE);
     init_pair(RED_PAIR, COLOR_RED, 0);
     init_pair(YELLOW_PAIR, COLOR_YELLOW, 0);
+}
 
-    // sets cursor state to invisible
-    curs_set(0);
+int main() {
+    // start ncurses
+    initscr();
+    noecho(); // don't print input
+    curs_set(0); // cursor invisible
+    create_colors(); // create color pairs
 
     // create game window
     WINDOW *game_win = create_game_window(
@@ -40,12 +40,10 @@ int main() {
         game_win_width
     );
 
-    wtimeout(game_win, 0);
+    wtimeout(game_win, 0); // don't stop the program on getch()
 
-    // create the 6 maps
-    map *maps[6];
+    // create the 6 maps and show splash screen
     create_maps(maps);
-    
     show_splash_screen(game_win);
 
     // start game
